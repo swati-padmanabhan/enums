49194075239bc3df2b500ec091486d25fe01308f
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-16 16:25:08 +0530

    initial commit of both files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    internal class Program
+    {
+        //enums are named constants.    
+        //An enum is a datatype that allows us to define a set of named constants
+        //We use enums when we have values that we know will not change
+
+        static void Main(string[] args)
+        {
+            Weather weather = Weather.Cloudy;
+            Console.WriteLine("\nToday's weather condition is: "+weather);
+
+            //To get the integer value, we need to explicitly convert to int
+            Console.WriteLine("\nToday's weather condition is: " +(int)weather);
+
+
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    enum Weather
+    {
+        //By default the each constant is assigned an integer value starting from zero
+        //And then incremented
+        Sunny,
+        Windy,
+        //We can also explicitly assign values if needed
+        Cloudy = 10,
+        Rainy,
+        Snowy
+    }
+}
